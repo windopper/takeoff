fdd92518079de2cd4a95da74f4cf0edb90728be1
windopper <open0916@naver.com>
2025-06-22 22:00:47 +0900

    feat: add Naver site verification to metadata

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -18,6 +18,9 @@ export const metadata: Metadata = {
   verification: {
     google: "d8Ovesw9e_k8y4kyS7FT6fTDVniHGZ46W5Dzo_pGlLk"
   },
+  other: {
+    "naver-site-verification": "91d079d1884e5ac2ed81c793a4ac22c8dad11413"
+  },
   category: "technology",
   description: "Takeoff는 AI/SW/CS 관련 아티클을 자동으로 정리 및 게시하는 서비스입니다"
 };
