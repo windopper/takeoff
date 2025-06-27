1629e91132ce300147a06de6ff03813937530b28
windopper <open0916@naver.com>
2025-06-27 13:33:44 +0900

    feat: update FRONTEND_URL to new domain for backend integration

--- backend/src/constants.ts
+++ backend/src/constants.ts
@@ -2,7 +2,7 @@ export const HACKERNEWS_RSS_BEST_URL = 'https://hnrss.org/best';
 export const HACKERNEWS_RSS_NEWEST_URL = 'https://hnrss.org/newest';
 
 export const PUBLIC_URL = "https://takeoff-backend.kamilereon.workers.dev";
-export const FRONTEND_URL = "https://takeoff-one.vercel.app";
+export const FRONTEND_URL = "https://ai-takeoff.dev/";
 
 export const POST_EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 14; // 14 days
 export const VECTORIZE_DELAY = 13000;
